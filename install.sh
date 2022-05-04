fb133c9719e76058cd595c3c7ed26ccbda7c0bbe
David Torrell <david@ciber.cat>
2022-05-04 21:53:17 +0200

    fix release

--- install.sh
+++ install.sh
@@ -20,51 +20,57 @@ if [ "$bash" = true ]; then
 fi
 
 if [ "$wget" = true ]; then
-	mv -f ~/.wgetrc ~/$(BACKUP_DIR) 2> /dev/null || true
+	BACKUP_DIR='.wget-backup'.`date +%Y-%m-%d.%H:%M:%S`
+	mkdir ~/$BACKUP_DIR
+
+	mv -f ~/.wgetrc ~/$BACKUP_DIR 2> /dev/null || true
 	ln -s  ~/.ciber_dotfiles/shell/wgetrc ~/.wgetrc
 fi
 
 if [ "$screen" = true ]; then
-	mv -f ~/.screenrc ~/$(BACKUP_DIR) 2> /dev/null || true
+	BACKUP_DIR='screen-backup'.`date +%Y-%m-%d.%H:%M:%S`
+	mkdir ~/$BACKUP_DIR
+	mv -f ~/.screenrc ~/$BACKUP_DIR 2> /dev/null || true
 	ln -s  ~/.ciber_dotfiles/shell/screenrc ~/.screenrc
 fi
 
-if [ "$curl" = true ]; then 
-    mv -f ~/.curlrc ~/$(BACKUP_DIR) 2> /dev/null || true
+if [ "$curl" = true ]; then
+	BACKUP_DIR='.curl-backup'.`date +%Y-%m-%d.%H:%M:%S` 
+	mkdir ~/$BACKUP_DIR
+    mv -f ~/.curlrc ~/$BACKUP_DIR 2> /dev/null || true
     ln -s  ~/.ciber_dotfiles/shell/curlrc ~/.curlrc
 fi
 
 if [ "$editorconfig" = true ]; then
-    mv -f ~/.editorconfig ~/$(BACKUP_DIR) 2> /dev/null || true
+	BACKUP_DIR='.editorconfig-backup'.`date +%Y-%m-%d.%H:%M:%S`
+	mkdir ~/$BACKUP_DIR
+    mv -f ~/.editorconfig ~/$BACKUP_DIR 2> /dev/null || true
 	ln -s  ~/.ciber_dotfiles/shell/editorconfig ~/.editorconfig
 fi
 
 if [ "$tmux" = true ]; then
 	command -v tmux >/dev/null 2>&1 || { echo >&2 "tmux is not installed.  Aborting."; exit 1; }
-	$(eval NOW := $(shell date +%Y-%m-%d.%H:%M:%S))
-	$(eval BACKUP_DIR := $(shell echo ".tmux-backup.$(NOW).$$$$"))
-	mkdir ~/$(BACKUP_DIR)
-	mv -f ~/.tmux* ~/$(BACKUP_DIR) 2> /dev/null || true
+	BACKUP_DIR='.tmux-backup'.`date +%Y-%m-%d.%H:%M:%S`
+	mkdir ~/$BACKUP_DIR
+	mv -f ~/.tmux* ~/$BACKUP_DIR 2> /dev/null || true
 
 	ln -s ~/.ciber_dotfiles/tmux/tmux.conf ~/.tmux.conf
 fi
 
 if [ "$vim" = true ]; then
 	command -v vi >/dev/null 2>&1 || { echo >&2 "vi is not installed.  Aborting."; exit 1; }
-	$(eval NOW := $(shell date +%Y-%m-%d.%H:%M:%S))
-	$(eval BACKUP_DIR := $(shell echo ".vim-backup.$(NOW).$$$$"))
-	mkdir ~/$(BACKUP_DIR)
-	mv -f ~/.vim* ~/$(BACKUP_DIR) 2> /dev/null || true
+	BACKUP_DIR='.vim-backup'.`date +%Y-%m-%d.%H:%M:%S`
+	mkdir ~/$BACKUP_DIR
+	mv -f ~/.vim* ~/$BACKUP_DIR 2> /dev/null || true
 
 	bash ~/.ciber_dotfiles/vim_runtime/install_awesome_vimrc.sh
 fi
 
 if [ "$git" = true ]; then
-	$(eval NOW := $(shell date +%Y-%m-%d.%H:%M:%S))
-	$(eval BACKUP_DIR := $(shell echo ".git-backup.$(NOW).$$$$"))
-	mkdir ~/$(BACKUP_DIR)
-	mv -f ~/.gitconfig ~/$(BACKUP_DIR) 2> /dev/null || true
-	mv -f ~/.gitignore ~/$(BACKUP_DIR) 2> /dev/null || true
+	BACKUP_DIR='.git-backup'.`date +%Y-%m-%d.%H:%M:%S`
+	mkdir ~/$BACKUP_DIR
+	mv -f ~/.gitconfig ~/$BACKUP_DIR 2> /dev/null || true
+	mv -f ~/.gitignore ~/$BACKUP_DIR 2> /dev/null || true
 	
 	cp -f ~/.ciber_dotfiles/git/gitconfig ~/.gitconfig
 	cp -f ~/.ciber_dotfiles/git/gitignore ~/.gitignore
